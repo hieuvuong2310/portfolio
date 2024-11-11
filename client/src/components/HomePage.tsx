460c4fb913d9998ebd42902bd9f4b692cad84b16
Trong Hieu Vuong <hieuvg02@gmail.com>
2024-11-11 14:54:51 -0800

    Add react app

---
+++ client/src/components/HomePage.tsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+
+export default function HomePage() { 
+    return (
+        <>
+        <div className="flex p-5">
+            <h1 className="flex-1 text-lg font-bold">Welcome to my portfolio page</h1>
+            <h1 className="flex-1 text-lg font-bold">Trong Hieu Vuong</h1>
+            <h1 className="flex-2 text-lg font-bold">Test</h1>
+        </div>
+        
+        
+        </>
+    )
+}
