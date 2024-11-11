bff14339ba8aa226789131a10510bcc9bdcfecd5
Trong Hieu (Lucas) Vuong <88293742+hieuvuong2310@users.noreply.github.com>
2024-11-11 15:58:07 -0800

    test ci/cd (#2)

--- personal-portfolio/src/app/home/HomePage.tsx
+++ personal-portfolio/src/app/home/HomePage.tsx
@@ -3,6 +3,7 @@ function HomePage() {
         <div>
             <h1>Home Page</h1>
             <h2>Welcome to my personal portfolio</h2>
+            <h2>My name is Lucas Vuong</h2>
         </div>
     )
 };
