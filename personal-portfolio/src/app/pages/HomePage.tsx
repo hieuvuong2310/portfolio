c5f4afbbd1bad0b0bd76b3d819abee0bbc6723f3
Trong Hieu Vuong <hieuvg02@gmail.com>
2024-11-13 04:42:13 -0800

    Fix lint

--- personal-portfolio/src/app/pages/HomePage.tsx
+++ personal-portfolio/src/app/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ResumeButton from '../components/ResumeButton';
 import Character from '../img/main.png';
 import Image from 'next/image';
