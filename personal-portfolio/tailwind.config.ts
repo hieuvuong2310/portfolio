0edabdceca33897ea081848e0ee9f666d44a8cc8
Trong Hieu Vuong <hieuvg02@gmail.com>
2024-11-13 23:34:00 -0800

    Fix npm run build

--- personal-portfolio/tailwind.config.ts
+++ personal-portfolio/tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import type { PluginAPI } from "tailwindcss/types/config";
 
 export default {
   content: [
@@ -30,7 +31,7 @@ export default {
     },
   },
   plugins: [
-    function ({ addUtilities }) {
+    function ({ addUtilities }: PluginAPI) {
       addUtilities({
         '.text-stroke': {
           '-webkit-text-stroke-width': '1px',
