7c757522c3832a2f3889479a6e7ff1609aa9042c
Trong Hieu (Lucas) Vuong <88293742+hieuvuong2310@users.noreply.github.com>
2024-11-13 23:18:20 -0800

    Reformat main page (#8)

--- personal-portfolio/src/app/components/ResumeButton.tsx
+++ personal-portfolio/src/app/components/ResumeButton.tsx
@@ -4,11 +4,11 @@ function ResumeButton() {
   return (
     <button className="w-[20%] h-[4%] bg-accent rounded-[20px]">
         <div className="flex flex-col items-center justify-center">
-            <div className="flex pl-10 pr-10">
+            <div className="flex pl-10 pr-10 items-center">
                 <div className="pr-2">
                     <Image src={Download} width={24} height={24} alt="download"/>
                 </div>
-                <div>
+                <div className="text-[20px] tracking-widest">
                     Resume
                 </div>
                 
--- personal-portfolio/src/app/pages/HomePage.tsx
+++ personal-portfolio/src/app/pages/HomePage.tsx
@@ -4,9 +4,11 @@ import Character from '../img/main.png';
 import Image from 'next/image';
 function HomePage() {
     return (
-        <div className="flex flex-col items-center justify-center h-screen">
-            <h1 className="text-[150px] font-primary text-primary">Lucas Vuong</h1>
-            <h1 className='font-accent'>DevOps, Fullstack Engineer</h1>
+        <div className="flex flex-col items-center h-screen space-y-10">
+            <div className='flex flex-col items-center space-y-[-2px]'>
+                <h1 className="text-[150px] font-primary text-primary text-stroke-3 text-stroke-black">Lucas Vuong</h1>
+                <h1 className='text-[30px] font-accent'>DevOps, Fullstack Engineer</h1>
+            </div>
             <Image src={Character} width={150} height={150} alt="character"/>
             <ResumeButton />
         </div>
