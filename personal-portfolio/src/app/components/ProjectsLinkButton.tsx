08a6061274325f82b2085ea3efc730f3bed5efe3
Trong Hieu Vuong <hieuvg02@gmail.com>
2024-11-17 12:09:55 -0800

    Add type back

--- personal-portfolio/src/app/components/ProjectsLinkButton.tsx
+++ personal-portfolio/src/app/components/ProjectsLinkButton.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import Github from "../img/github.png";
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 interface ProjectsLinkButtonProps {
     name: string;
     link: string;
