133363bdfdc6e7601e8c19b3ea862f4563ebdaa5
Trong Hieu (Lucas) Vuong <88293742+hieuvuong2310@users.noreply.github.com>
2024-11-17 16:22:48 -0800

    Add professional pages (#12)

---
+++ personal-portfolio/src/app/server/src/helper/getProfessionals.ts
@@ -0,0 +1,28 @@
+import professionalsData from "../../../resources/professionals.json";
+
+interface ProfessionalsProps {
+    icon: string;
+    time: string;
+    company: string;
+    position: string;
+    description: string;
+    color: string;
+}
+const getProfessionals = () => {
+    const professionals: ProfessionalsProps[] = [];
+    for (let i = 0; i < professionalsData.length; i++) {
+        const professional: ProfessionalsProps = {
+            icon: professionalsData[i]?.icon,
+            time: professionalsData[i]?.time,
+            company: professionalsData[i]?.company,
+            position: professionalsData[i]?.position,
+            description: professionalsData[i]?.description,
+            color: professionalsData[i]?.color
+        };
+        professionals.push(professional);
+    }
+    // console.log(professionals);
+    return professionals;
+};
+
+export default getProfessionals;
