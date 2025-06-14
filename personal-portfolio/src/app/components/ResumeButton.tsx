aab6e1fb24a32213989d069def1b3e92442efa6b
Trong Hieu (Lucas) Vuong <88293742+hieuvuong2310@users.noreply.github.com>
2025-06-13 22:43:44 -0700

    Update ResumeButton.tsx

--- personal-portfolio/src/app/components/ResumeButton.tsx
+++ personal-portfolio/src/app/components/ResumeButton.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 function ResumeButton() {
   return (
         <button className="w-[20%] h-[4%] bg-accent rounded-[20px]">
-            <Link href="https://firebasestorage.googleapis.com/v0/b/personal-web-4022f.firebasestorage.app/o/TrongHieu_Vuong_Resume_2025.pdf?alt=media&token=b0db1034-cf8f-4d51-b8da-701ef23f3f13" target="_blank">
+            <Link href="https://firebasestorage.googleapis.com/v0/b/personal-web-4022f.firebasestorage.app/o/Resume_LucasVuong.pdf?alt=media&token=929135d7-f71b-4cc4-9d4c-fcdfe1015c6a" target="_blank">
                 <div className="flex flex-col items-center justify-center">
                     <div className="flex pl-10 pr-10 items-center">
                         <div className="pr-2">
